d64eadb5c07365424bd3546bcb35c42e76bb0e3d
José Miguel <clasiccode@gmail.com>
2025-06-21 19:40:33 +0200

    Create InjectAttribute.cs

---
+++ WinForms.Extensions.DependencyInjection/Attributes/InjectAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WinForms.Extensions.DependencyInjection.Attributes
+{
+    /// <summary>
+    /// Marca una propiedad para que sea inyectada automáticamente desde el contenedor DI.
+    /// Útil cuando el constructor no puede usarse (por ejemplo, en formularios diseñados por WinForms Designer).
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class InjectAttribute : Attribute
+    {
+    }
+}
