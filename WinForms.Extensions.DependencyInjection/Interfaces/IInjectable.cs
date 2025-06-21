4b859a7085ad3d5f013246ea30b3c51821475ea2
José Miguel <clasiccode@gmail.com>
2025-06-21 19:40:38 +0200

    Create IInjectable.cs

---
+++ WinForms.Extensions.DependencyInjection/Interfaces/IInjectable.cs
@@ -0,0 +1,14 @@
+﻿namespace WinForms.Extensions.DependencyInjection.Interfaces
+{
+    /// <summary>
+    /// Interfaz que puede implementar cualquier clase para recibir una llamada
+    /// después de que se hayan resuelto las dependencias, ideal para inicializaciones.
+    /// </summary>
+    public interface IInjectable
+    {
+        /// <summary>
+        /// Método llamado tras la inyección de dependencias.
+        /// </summary>
+        void OnInjected();
+    }
+}
